fb9f15e1f153c8a122ebf187a3c9c40974849ad6
Vasile iulian <viulian@hotmail.de>
2017-11-03 06:12:33 +0100

    Seed20171103

---
+++ ShortLifeFileCapture/Forms/MonitorDir.Designer.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ShortLifeFileCapture.Forms
+{
+    partial class MonitorDir
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialogExampleFormBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.folderBrowserDialogExampleFormBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(32, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Dir:";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(60, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(453, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // folderBrowserDialogExampleFormBindingSource
+            // 
+            this.folderBrowserDialogExampleFormBindingSource.DataSource = typeof(ShortLifeFileCapture.Classes.FolderBrowserDialogExampleForm);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(501, 61);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Start Monitoring";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // MonitorDir
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 109);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "MonitorDir";
+            this.Text = "MonitorDir";
+            ((System.ComponentModel.ISupportInitialize)(this.folderBrowserDialogExampleFormBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        [STAThreadAttribute]
+        static void Main()
+        {
+            Application.Run(new MonitorDir());
+        }
+
+        private OpenFileDialog openFileDialog1;
+        private Button button1;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private TextBox textBox1;
+        private BindingSource folderBrowserDialogExampleFormBindingSource;
+        private Button button2;
+    }
+}
---
+++ ShortLifeFileCapture/Forms/MonitorDir.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShortLifeFileCapture.Forms
+{
+    public partial class MonitorDir : Form
+    {
+        bool Start = false;
+
+        List<String> FileList = new List<string>();
+        
+        public MonitorDir()
+        {
+            InitializeComponent();
+            textBox1.Text = "C:\\temp";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                this.textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var curDir = textBox1.Text;
+            if (Start == false)
+                Start = true;
+            else
+                Start = false;
+            var files = Directory.GetFiles(this.textBox1.Text, "*", SearchOption.TopDirectoryOnly).OrderBy(f => new FileInfo(f).CreationTime);
+            foreach (var file in files ) { FileList.Add(file); }
+            while (Start)
+            {
+                files = Directory.GetFiles(this.textBox1.Text, "*", SearchOption.TopDirectoryOnly).OrderBy(f => new FileInfo(f).CreationTime);
+                if ( files.Count() != FileList.Count)
+                {
+                    // Copy file in temp
+                    File.Copy(Path.Combine(curDir, files.Last()), Path.Combine("C:\\temp", files.Last()),true);
+                    FileList.Add(files.Last());
+                }
+                    
+            }
+
+        }
+    }
+}
